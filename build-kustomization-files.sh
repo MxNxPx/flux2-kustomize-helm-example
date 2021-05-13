b36ae93bdb209e3b634fd6d547ea294694e3f0c2
michael <mxnxpx@gmail.com>
2021-05-13 07:29:55 -0400

    some updates

--- build-kustomization-files.sh
+++ build-kustomization-files.sh
@@ -1,20 +1,31 @@
 #!/bin/bash
+cmd_pattern="^#(touch|kubectl|kubeseal)"
 
+# find and loop thru all kustomization.yaml files
 for i in $(find . -type f -name 'kustomization.yaml'); do
-  SAVEIFS=$IFS
-  IFS=$'\n'
+  # set current dir to later return back to
+  start_dir=$(pwd)
 
-  command_list=$(grep "^# kustomize" $i 2>/dev/null)
-  if [[ ! -z "${command_list}" ]]; then
+  # look for a command pattern in the file
+  command_list=$(egrep "$cmd_pattern" $i 2>/dev/null)
+
+  # if command pattern not empty (aka finds a match) then perform operations
+  if [[ ! -z "$command_list" ]]; then
     echo "[*] updating $i"
+
+    # change to the dir where the file is stored
     cd $(dirname $i)
+
+    # remove portion of the file below "resources:" to the end
     sed -i '/^resources:$/q' kustomization.yaml
 
-    for cmd in $command_list; do
-      echo $cmd | sed 's/^#//' | bash
-    done
+    # read in the list of commands and execute them
+    while IFS= read -r cmd; do
+      eval $(echo "$cmd" | sed 's/^#//g')
+    done <<< "$command_list"
 
+    # return to starting dir before going to the next file in the loop
+    cd $start_dir
   fi
 
 done
-IFS=$SAVEIFS
