517f24efe6079b884df4119143c59eaf03d63639
michael <mxnxpx@gmail.com>
2021-06-25 07:29:43 -0400

    testing bring your own cert for sealed-secretsush

--- clusters/_shared_/sealed-secrets-base/certs.sh
+++ clusters/_shared_/sealed-secrets-base/certs.sh
@@ -1,5 +1,5 @@
-export PRIVATEKEY="mytls.key"
-export PUBLICKEY="mytls.crt"
+export PRIVATEKEY="tls.key"
+export PUBLICKEY="tls.crt"
 export SECRETNAME="mycustomkeys"
 
 openssl req -x509 -nodes -newkey rsa:4096 -keyout "$PRIVATEKEY" -out "$PUBLICKEY" -subj "/CN=sealed-secret/O=sealed-secret"
